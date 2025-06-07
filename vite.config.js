837cb16cdb20413d6a7d034e913e8af18e1ffcaf
Melvin Peralta <melvin.a.p.cruz@gmail.com>
2025-06-07 14:47:04 -0400

    Add src directory and update build configuration

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,13 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
+      input: {
+        main: './index.html'
+      },
       output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
     }
   },
